6cfd01e917c135ae248d99aff859f310e13ff692
mule <belmeetmule@gmail.com>
2022-11-03 00:33:29 +0300

    add table animal schema

--- schema.sql
+++ schema.sql
@@ -1,11 +1,11 @@
 /* Database schema to keep the structure of entire database. */
 
 /* schema for animals table. */
-vet_clinic=# CREATE TABLE IF NOT EXISTS animals (
-vet_clinic(# id SERIAL PRIMARY KEY NOT NULL,
-vet_clinic(# name VARCHAR(30) NOT NULL,
-vet_clinic(# date_of_birth DATE DEFAULT CURRENT_DATE,
-vet_clinic(# escape_attempt INT DEFAULT 0,
-vet_clinic(# neutered BOOLEAN,
-vet_clinic(# weight_kg DECIMAL
-vet_clinic(# );
+CREATE TABLE IF NOT EXISTS animals (
+id integer primary key generated always as identity,
+name VARCHAR(30) NOT NULL,
+date_of_birth DATE DEFAULT CURRENT_DATE,
+escape_attempts INT DEFAULT 0,
+neutered BOOLEAN,
+weight_kg DECIMAL
+);
