4993acf55b64dac670af19cf4d4063a4c3037cac
mule <belmeetmule@gmail.com>
2022-11-03 01:40:04 +0300

    update schema

--- schema.sql
+++ schema.sql
@@ -1,7 +1,10 @@
 /* Database schema to keep the structure of entire database. */
 
 /* schema for animals table. */
-CREATE TABLE IF NOT EXISTS animals (
+
+DROP TABLE IF EXISTS animals;
+
+CREATE TABLE animals (
 id integer primary key generated always as identity,
 name VARCHAR(30) NOT NULL,
 date_of_birth DATE DEFAULT CURRENT_DATE,
